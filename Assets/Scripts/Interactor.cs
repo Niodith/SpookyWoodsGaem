7e326c548b00013ea8d3fb4946f6d3660d11f631
Kjell <the_underpro@gmx.net>
2022-12-20 14:50:05 +0100

    Added first interactable interface
    nothing implemented and added a bunch of scenes to gitignore though

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionPointRadius;
+    [SerializeField] private LayerMask _interactableMask;
+
+    private readonly Collider[] _colliders = new Collider[3];
+    [SerializeField] private int _numFound;
+}
